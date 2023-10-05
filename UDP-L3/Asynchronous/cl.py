1848025ccc692abef4c94e47fc05b464adb75770
VaibhavBNagrale <112001046@smail.iitpkd.ac.in>
2023-10-06 01:07:52 +0530

    created async client and server

---
+++ UDP-L3/Asynchronous/cl.py
@@ -0,0 +1,97 @@
+import asyncio
+import asyncudp
+import aioconsole
+import random
+import struct
+import os
+import sys
+import signal
+
+# Define constants for command header fields
+CMD_SETUP_CONNECTION = 0
+CMD_DATA = 1
+CMD_ALIVE = 2
+CMD_GOODBYE = 3
+
+# Initialize the sequence number and session_id
+delay = 50
+sequence_number = 0
+session_id = random.randint(0, 0xFFFFFFFF)
+
+# Check if the received data has the correct header structure
+header_size = struct.calcsize("!HBBII")
+# cmd = await aioconsole.ainput()
+
+async def send_messages(client_socket, server_address):
+    global sequence_number,session_id, timer
+    # client_socket.sendto(struct.pack('!HBBII', 0xC461, 1, CMD_SETUP_CONNECTION, sequence_number, session_id), server_address)
+    while True:
+        sequence_number += 1
+        # user_input = sys.stdin.readline().rstrip()
+        try:
+            user_input = await aioconsole.ainput("")
+        except EOFError:
+            user_input = "eof"
+        
+        timer = asyncio.create_task(start_timer(client_socket))
+        
+        if user_input == 'q' or user_input == 'eof':
+            # Send a goodbye message (4) to the server and exit
+            client_socket.sendto(struct.pack('!HBBII', 0xC461, 1, CMD_GOODBYE, sequence_number, session_id) + user_input.encode(), server_address)
+            break
+        else:
+            # Send a data message (2) to the server with the current sequence number
+            client_socket.sendto(struct.pack('!HBBII', 0xC461, 1, CMD_DATA, sequence_number, session_id) + user_input.encode(), server_address)
+        
+
+# Function to handle receiving messages from the server
+async def receive_messages(client_socket):
+    global timer
+    while True:
+        data, _ = await client_socket.recvfrom()
+        _, _, command, _, _ = struct.unpack('!HBBII', data[:header_size])
+            
+        if timer:
+            timer.cancel()
+        
+        if command == CMD_SETUP_CONNECTION:
+            print("HELLO from Server")
+        elif command == CMD_ALIVE:
+            print("ALIVE from Server")
+        elif command == CMD_GOODBYE:
+            print("GOODBYE from Server")
+            break
+        else:
+            print("Unexpected response from server.")
+
+# Send a connection setup message (0) to the server
+async def connect_server(client_socket, server_address):
+    global sequence_number, timer
+    timer = asyncio.create_task(start_timer(client_socket))
+    client_socket.sendto(struct.pack('!HBBII', 0xC461, 1, CMD_SETUP_CONNECTION, sequence_number, session_id), server_address)
+
+async def start_timer(client_socket):
+    await asyncio.sleep(delay)
+    print("Timeout occurred.")
+    
+    client_socket.close()
+    myPid = os.getpid()
+    os.kill(myPid, signal.SIGTERM)
+    sys.exit()
+
+async def main():
+    server_address = ('127.0.0.1', 50001)
+    client_socket = await asyncudp.create_socket(remote_addr=server_address)
+    # await connect_server(client_socket, server_address)
+
+    connect_task = asyncio.create_task(connect_server(client_socket, server_address))
+
+    send_task = asyncio.create_task(send_messages(client_socket, server_address))
+    recieve_task = asyncio.create_task(receive_messages(client_socket, ))
+
+    await asyncio.gather(connect_task, recieve_task)
+    
+    # client_socket.close()
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ UDP-L3/Asynchronous/sr.py
@@ -0,0 +1,166 @@
+import asyncio
+import asyncudp
+import aioconsole
+import struct
+import os
+import sys
+import signal
+import time
+
+# Define constants for command header fields
+CMD_SETUP_CONNECTION = 0
+CMD_DATA = 1
+CMD_ALIVE = 2
+CMD_GOODBYE = 3
+
+delay = 100
+active_sessions = {}        # stores tuple (session_id, client_address)
+message_tuple = {}          # stores message for given session_id in Queue
+# last_activity_time = {}     # stores last activity time for session_id
+# storing sequence number and time wrt session_id
+sequence_dict = {}
+# timeout_dict = {}
+
+# Check if the received data has the correct header structure
+header_size = struct.calcsize("!HBBII")
+
+# Function to handle incoming messages from a client
+async def handle_client(session_id, server_socket):
+    global timer
+    # get active session data
+    client_address = active_sessions[session_id]
+    expected_sequence_number = 0
+    while True:
+        # get data from thread
+        _, _, command, sequence_number, session_id, message = await message_tuple[session_id].get()
+
+        if sequence_number > expected_sequence_number:
+            # while True:
+            #     print("===========================1")
+            for seq in range(expected_sequence_number, sequence_number-1):
+                print(f'0x{session_id:08x} [{seq}] [Lost packet]')
+            expected_sequence_number = sequence_number + 1 
+
+        # Check for duplicate packets
+        elif sequence_number == expected_sequence_number-1:
+            # while True:
+            #     print("===========================2")
+            print(f'0x{session_id:08x} [Duplicate packet] ')
+            continue
+
+        elif sequence_number < expected_sequence_number - 1:
+            server_socket.sendto(struct.pack('!HBBII', 0xC461, 1, CMD_GOODBYE, sequence_number, session_id), client_address)
+            # while True:
+            #     print("===========================3")
+            # delete proper data wrt session_id
+            del active_sessions[session_id]
+            # del last_activity_time[session_id]
+            break
+
+        else:
+            expected_sequence_number += 1
+
+        # Process the packet based on the command
+        if command == CMD_SETUP_CONNECTION: # Reply with 0 for connection setup
+            print(f"{hex(session_id)} [{sequence_number}] Session created")
+            server_socket.sendto(struct.pack('!HBBII', 0xC461, 1, CMD_SETUP_CONNECTION, sequence_number, session_id), client_address)
+            timer = asyncio.create_task(start_timer(server_socket, session_id))
+
+        elif command == CMD_DATA:           # Reply with 2 for Alive
+            print(f"{hex(session_id)} [{sequence_number}] {message.decode()}")
+            server_socket.sendto(struct.pack('!HBBII', 0xC461, 1, CMD_ALIVE, sequence_number, session_id), client_address)
+            if timer:
+                timer.cancel()
+            timer = asyncio.create_task(start_timer(server_socket, session_id))
+
+        elif command == CMD_GOODBYE:        # Reply with 3 for Goodbye
+            # timeout_dict[session_id].cancel()
+            
+            print(f"{hex(session_id)} [{sequence_number}] GOODBYE from client.")
+            server_socket.sendto(struct.pack('!HBBII', 0xC461, 1, CMD_GOODBYE, sequence_number, session_id), client_address)
+            
+            # delete proper data wrt session_id
+            del active_sessions[session_id]
+
+            if timer:
+                timer.cancel()
+            # del last_activity_time[session_id]
+            
+            print(f"{hex(session_id)} Session closed")
+            break
+
+async def quit_server(server_socket):
+    while True:
+        user_input = await aioconsole.ainput()
+        if user_input == 'q':
+            # Send a goodbye message to all connected clients
+            for session_id, client_address in active_sessions.items():
+                server_socket.sendto(struct.pack('!HBBII', 0xC461, 1, CMD_GOODBYE, 0, session_id), client_address)
+            
+            # clear dicts
+            active_sessions.clear()
+            # last_activity_time.clear()
+
+            # kill process
+            server_socket.close()
+            myPid = os.getpid()
+            os.kill(myPid, signal.SIGTERM)
+            sys.exit()
+            break
+        time.sleep(1)
+
+async def get_packet(server_socket):
+    # global timeout_thread
+    while True:
+        # Receive data from the client
+        data, client_address = await server_socket.recvfrom()
+
+        # Unpack the header fields
+        magic, version, command, sequence_number, session_id = struct.unpack('!HBBII', data[:header_size])
+
+        sequence_dict[session_id] = sequence_number
+        
+        # Check magic and version
+        if magic != 0xC461 or version != 1:
+            print(f"Received packet with invalid magic or version from {client_address}. Discarding...")
+            continue
+        
+        # if command == CMD_SETUP_CONNECTION: 
+        #     print(f"{session_id} {sequence_dict[session_id]} connecion reiceved here1")
+
+        # Process the packet based on the session ID
+        if session_id not in active_sessions:
+            # Check for new connection from client
+            if command != CMD_SETUP_CONNECTION or sequence_dict[session_id] != 0:
+                continue
+
+            active_sessions[session_id] = client_address
+
+            # message_tuple stores messages and session_dict store client addres for each session id
+            message_tuple[session_id] = asyncio.Queue()
+
+            asyncio.create_task(handle_client(session_id, server_socket))
+        # Inserting message to respective session_id into Queue
+        await message_tuple[session_id].put((magic, version, command, sequence_dict[session_id], session_id, data[header_size:]))
+
+async def start_timer(server_socket, session_id):
+    await asyncio.sleep(delay)
+    if session_id in active_sessions:
+        print("Timeout occurred.")
+        server_socket.sendto(struct.pack('!HBBII', 0xC461, 1, CMD_GOODBYE, sequence_dict[session_id], session_id) + 'q'.encode(), active_sessions[session_id])
+        del active_sessions[session_id]
+
+async def main():
+    server_address = ('127.0.0.1', 50001)
+    server_socket = await asyncudp.create_socket(local_addr=server_address)
+
+    quit_task = asyncio.create_task(quit_server(server_socket))
+    get_tasks = asyncio.create_task(get_packet(server_socket))
+
+    await asyncio.gather(quit_task, get_tasks)
+
+# asyncio.run(main())
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    loop.close()
